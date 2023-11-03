41ed085bf765bed814052b275ba200de83375522
Amp-Apirak <apirak.ba@gmail.com>
2023-11-03 20:54:11 +0700

    B

--- db_pms.sql
+++ db_pms.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 03, 2023 at 11:41 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.0.23
+-- Generation Time: Nov 03, 2023 at 02:53 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `category` (
   `category_id` int(11) NOT NULL,
-  `category_name` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL
+  `category_name` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -50,7 +50,7 @@ INSERT INTO `category` (`category_id`, `category_name`) VALUES
 
 CREATE TABLE `contact` (
   `contact_id` int(11) NOT NULL,
-  `contact_name` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL
+  `contact_name` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -71,7 +71,7 @@ INSERT INTO `contact` (`contact_id`, `contact_name`) VALUES
 
 CREATE TABLE `items` (
   `items_id` int(11) NOT NULL,
-  `items_name` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL
+  `items_name` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -94,7 +94,7 @@ INSERT INTO `items` (`items_id`, `items_name`) VALUES
 
 CREATE TABLE `service` (
   `service_id` int(11) NOT NULL,
-  `service_name` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL
+  `service_name` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -114,12 +114,25 @@ INSERT INTO `service` (`service_id`, `service_name`) VALUES
 
 CREATE TABLE `tb_log` (
   `log_id` int(11) NOT NULL,
-  `task` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `result` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `v_status` varchar(1000) NOT NULL,
+  `add_task` varchar(1000) NOT NULL,
   `date_edit` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `staff_edit` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `staff_edit` varchar(1000) NOT NULL,
   `work_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tb_log`
+--
+
+INSERT INTO `tb_log` (`log_id`, `v_status`, `add_task`, `date_edit`, `staff_edit`, `work_id`) VALUES
+(1, 'On Process', '1', '2023-11-03 11:17:20', 'พี่ขวัญ', 61),
+(2, 'Done', '1', '2023-11-03 11:17:28', 'พี่ขวัญ', 61),
+(4, '', '1', '2023-11-03 10:57:19', 'พี่ขวัญ', 59),
+(5, '', '1', '2023-11-03 10:58:43', 'พี่ขวัญ', 59),
+(6, '', '', '2023-11-03 11:01:32', 'พี่ขวัญ', 59),
+(7, '', 'asdasdasdasdasdasdasdas', '2023-11-03 11:02:14', 'คุณอาทิตย์ (พี่เหน่ง)', 59),
+(8, 'On Process', 'efdsgsegdsffgdsgdfsggdsfgdsfgsdfgds', '2023-11-03 11:24:35', 'คุณอภิรักษ์ (แอมป์)', 61);
 
 -- --------------------------------------------------------
 
@@ -129,21 +142,21 @@ CREATE TABLE `tb_log` (
 
 CREATE TABLE `work` (
   `work_id` int(11) NOT NULL,
-  `project_name` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `work_type` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `service` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `category` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `items` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `subject` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `detail` varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `requester` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `project_name` varchar(1000) NOT NULL,
+  `work_type` varchar(500) NOT NULL,
+  `service` varchar(500) NOT NULL,
+  `category` varchar(500) NOT NULL,
+  `items` varchar(500) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `subject` varchar(500) NOT NULL,
+  `detail` varchar(1000) NOT NULL,
+  `requester` varchar(255) NOT NULL,
   `date_crt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `staff_crt` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `staff_crt` varchar(255) NOT NULL,
   `date_edit` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `staff_edit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `file_upfile` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'เก็บรูปภาพ',
-  `file_test` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `staff_edit` varchar(255) NOT NULL,
+  `file_upfile` varchar(255) NOT NULL COMMENT 'เก็บรูปภาพ',
+  `file_test` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -153,9 +166,9 @@ CREATE TABLE `work` (
 INSERT INTO `work` (`work_id`, `project_name`, `work_type`, `service`, `category`, `items`, `status`, `subject`, `detail`, `requester`, `date_crt`, `staff_crt`, `date_edit`, `staff_edit`, `file_upfile`, `file_test`) VALUES
 (57, 'Up Level (Phase 1)', 'Incident', 'Analyzer', 'Module', 'Add', 'On Process', 'แจ้งขอ เปลี่ยนรูปแบบการแสดง Error : Please fill in complete information เมือกด Add Analyzer ', 'แจ้งขอ เปลี่ยนรูปแบบการแสดง Error : Please fill in complete information เมือกด Add Analyzer ไม่ได้ Add Module*\r\nเมือกด Add Analyzer ไม่ได้ Add Module*  ให้เปลี่ยนเป็น  \" Please fill in \"Module information. ! \"\r\n\r\nแนะนำจ้าง outsource ในการ fix ครับ', 'คุณอภิรักษ์ (แอมป์)', '2023-11-01 16:32:40', 'คุณอภิรักษ์ (แอมป์)', '2023-11-01 16:32:40', 'พี่ขวัญ', 'Error_Add_module.jpg', ''),
 (58, 'Up Level (Phase 1)', 'Incident', 'Analyzer', 'Textfield *', 'Edit/Update', 'On Process', 'แจ้งขอ เปลี่ยนรูปแบบการแสดง Error : Please fill in complete information (Textfield one more)', 'แจ้งขอ เปลี่ยนรูปแบบการแสดง Error : Please fill in complete information\r\nเมือกด Add Analyzer ไม่ได้กรอกฟิลด์บังคับ ** ดังนี้ \r\n\r\nหากไมไ่ด้กรอกข้อมูลมากกว่า 1 ให้แสดง =  Error : Please fill in complete information\r\n \r\nกรณีลืมกรอกแค่ 1 ฟิวด์ \r\n\r\nแนะนำจ้าง outsource ในการ fix ครับ\r\nGeneXpert S/N* =  Error : Please fill in \"GeneXpert S/N information. !\"\r\nModel * =  Error : Please fill in \"Model information. !\"\r\nModule Code * =  Error : Please fill in \"Module Code information. !\"\r\nColor Code *  =  Error : Please fill in \"Color Code information. !\"\r\n', 'คุณอภิรักษ์ (แอมป์)', '2023-11-01 16:32:24', 'คุณอภิรักษ์ (แอมป์)', '2023-11-01 16:32:24', 'พี่ขวัญ', 'Error_Add_Text.jpg', ''),
-(59, 'PMS (Phase 1)', 'Service', 'Analyzer', 'Module', 'Edit/Update', 'Done', 'ขอบริการ แก้ไขสีพื้นหลัง สถานะ ', 'ขอบริการ แก้ไขสีพื้นหลัง สถานะ \"Do not use\" เป็นสีแดง\r\n\r\nแก้ไขเรียบร้อย', 'คุณภัทราอร (ซีน)', '2023-11-01 16:46:56', 'คุณอภิรักษ์ (แอมป์)', '2023-11-01 16:46:56', 'พี่ขวัญ', 'Change_color.jpg', ''),
+(59, 'PMS (Phase 1)', 'Service', 'Analyzer', 'Module', 'Edit/Update', 'Done', 'ขอบริการ แก้ไขสีพื้นหลัง สถานะ ', 'ขอบริการ แก้ไขสีพื้นหลัง สถานะ \"Do not use\" เป็นสีแดง\r\n\r\nแก้ไขเรียบร้อย', 'คุณภัทราอร (ซีน)', '2023-11-03 11:02:14', 'คุณอภิรักษ์ (แอมป์)', '2023-11-03 11:02:14', 'คุณอาทิตย์ (พี่เหน่ง)', 'Change_color.jpg', ''),
 (60, 'PMS (Phase 1)', 'Incident', 'Analyzer', 'Table List', 'Edit/Update', 'Done', 'ขอบริการ เพิ่มแท็กสีสถานะ ', 'ขอบริการ เพิ่มแท็กสีสถานะ \"Colum : Status Module\" หากมี Module เสีย ให้แสดงแทบสีติดสถานะ\r\nต้องปรับ 2 ที่ ครับ ทั้งหน้าบ้าน หลังบ้าน ต้องมีมา flag มาบอกว่ามันเสีย เพื่อแปลี่ยนสี\r\n\r\nแก้ไขแล้ว', 'คุณภัทราอร (ซีน)', '2023-11-03 10:35:15', 'คุณอภิรักษ์ (แอมป์)', '2023-11-03 10:35:15', 'พี่ขวัญ', 'Status_module.jpg', 'color.png'),
-(61, 'PMS (Phase 1)', 'Incident', 'Computer', 'Textfield *', 'Edit/Update', 'On Process', 'แจ้งขอ เปลี่ยนรูปแบบการแสดง Error : Please fill in complete information (Textfield one more)', 'แจ้งขอ เปลี่ยนรูปแบบการแสดง Error : Please fill in complete information\r\nเมือกด Add Computer Menu ไม่ได้กรอกฟิลด์บังคับ ** ดังนี้\r\n\r\nหากไมไ่ด้กรอกข้อมูลมากกว่า 1 ให้แสดง = Error : Please fill in complete information.\r\n\r\nกรณีลืมกรอกแค่ 1 ฟิวด์\r\nComputer Code/ Analyzer Code  = Error : Please fill in \"GeneXpert Serial Number information. \"\r\nComputer S/N * = Error : Please fill in \"Computer Serial Number information. \"\r\nOrganization * = Error : Please select in \"Organization information. \"\r\nLaboratory * = Error : Please select in \"Laboratory information. \"\r\n\r\nแนะนำจ้าง outsource ในการ fix ครับ', 'คุณภัทราอร (ซีน)', '2023-11-01 16:26:34', 'คุณภัทราอร (ซีน)', '2023-11-01 16:26:34', 'พี่ขวัญ', 'Com_tectfeild.jpg', ''),
+(61, 'PMS (Phase 1)', 'Incident', 'Computer', 'Textfield *', 'Edit/Update', 'On Process', 'แจ้งขอ เปลี่ยนรูปแบบการแสดง Error : Please fill in complete information (Textfield one more)', 'แจ้งขอ เปลี่ยนรูปแบบการแสดง Error : Please fill in complete information\r\nเมือกด Add Computer Menu ไม่ได้กรอกฟิลด์บังคับ ** ดังนี้\r\n\r\nหากไมไ่ด้กรอกข้อมูลมากกว่า 1 ให้แสดง = Error : Please fill in complete information.\r\n\r\nกรณีลืมกรอกแค่ 1 ฟิวด์\r\nComputer Code/ Analyzer Code  = Error : Please fill in \"GeneXpert Serial Number information. \"\r\nComputer S/N * = Error : Please fill in \"Computer Serial Number information. \"\r\nOrganization * = Error : Please select in \"Organization information. \"\r\nLaboratory * = Error : Please select in \"Laboratory information. \"\r\n\r\nแนะนำจ้าง outsource ในการ fix ครับ', 'คุณภัทราอร (ซีน)', '2023-11-03 11:19:30', 'คุณภัทราอร (ซีน)', '2023-11-03 11:19:30', 'คุณอภิรักษ์ (แอมป์)', 'Com_tectfeild.jpg', ''),
 (62, 'PMS (Phase 1)', 'Service', 'Platform', 'Table List', 'Add', 'On Process', 'ขอบริการ นับจำนวนแสดงรายการทั้งหมด (Count record)', 'ขอบริการ เพิ่มเติมตารางแสดงผลทุกหน้า ให้แสดงตัวเลข รายการที่แสดงทั้งหมด \r\n(Total Count Reord Table)\r\n\r\ncount นับต่อหน้า หรือ ที่มีทั้งหมดครับ \r\nต่อหนัา default 10 item per page  \r\n\r\nแสดงตามตัวอย่างในภาพ test นะครับ', 'คุณภัทราอร (ซีน)', '2023-11-03 10:12:11', 'คุณภัทราอร (ซีน)', '2023-11-03 10:12:11', 'พี่ขวัญ', 'com_count.jpg', 'table Count.png'),
 (63, 'PMS (Phase 1)', 'Service', 'Platform', 'Table List', 'Add', 'On Process', 'ขอบริการ เพิ่มเติมสามารถคลิกจัดเรียงหัวตาราง (A-Z, Z-A) ได้', 'ขอบริการ เพิ่มเติมสามารถคลิกจัดเรียงหัวตาราง (A-Z, Z-A) ได้ \r\n\r\nระบุให้ด้วยครับ field ไหนต้องการ sort\r\n\r\nเปิดทุก field ยกแว้น action', 'คุณภัทราอร (ซีน)', '2023-11-03 10:24:51', '', '2023-11-03 10:24:51', 'พี่ขวัญ', 'Unit.jpg', 'sort.png'),
 (64, 'PMS (Phase 1)', 'Service', 'Analyzer', 'Textfield *', 'Add', 'On Process', 'ขอบริการ เพิ่ม  ลิงค์ “Example pattern file for upload.” สำหรับดาวน์โหลด Templated.', 'ขอบริการ เพิ่ม  ลิงค์ “Example pattern file for upload.” สำหรับดาวน์โหลด Templated.\r\n\r\nต้องปรับ 2 ที่ ครับ ทั้งหน้าบ้าน หลังบ้าน เพิ่ม endpoint รับ file upload\r\n', 'คุณภัทราอร (ซีน)', '2023-11-01 16:35:17', 'คุณภัทราอร (ซีน)', '2023-11-01 16:35:17', 'พี่ขวัญ', 'Upload.jpg', '');
@@ -232,7 +245,7 @@ ALTER TABLE `service`
 -- AUTO_INCREMENT for table `tb_log`
 --
 ALTER TABLE `tb_log`
-  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `log_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `work`
