6028cc7e6b473b9ec1dd314ae3b6782f1ff07d7f
Amp-Apirak <apirak.ba@gmail.com>
2023-11-08 16:57:47 +0700

    SS

--- edit.php
+++ edit.php
@@ -100,34 +100,34 @@
                                         error_reporting(E_ALL);
                                         date_default_timezone_set("Asia/Bangkok");
                             
-                                        // $sToken = "8CyHEXNouMVT3mgLFBb8sw74DbEwkZ5lN6oabOQ0vk9";
-                                        // $sMessage = "".$staff_edit." **Update Ticket** \n\n";
+                                        $sToken = "ELxH8LTeDYRfZjPrRzVoDa7aVLHgN9d0VgifAJiKrQu";
+                                        $sMessage = "".$staff_edit." **Update Ticket** \n\n";
 
-                                        // $sMessage .= "Category: ".$category." \n";
-                                        // $sMessage .= "Type: ".$work_type." \n";
-                                        // $sMessage .= "Items: ".$items." \n\n";
-                                        // $sMessage .= "-------------------------- \n";
-                                        // $sMessage .= "Status : ".$status."\n";
-                                        // $sMessage .= "-------------------------- \n";
-                                        // $sMessage .= "Owner: ".$requester." \n";
-                                        // $sMessage .= "Subject : ".$subject."\n\n";
-                                        // $sMessage .= "-------------------------- \n";
-                                        // $sMessage .= "คำแนะ/แก้ไข : ".$add_task."\n\n";
+                                        $sMessage .= "Category: ".$category." \n";
+                                        $sMessage .= "Type: ".$work_type." \n";
+                                        $sMessage .= "Items: ".$items." \n\n";
+                                        $sMessage .= "-------------------------- \n";
+                                        $sMessage .= "Status : ".$status."\n";
+                                        $sMessage .= "-------------------------- \n";
+                                        $sMessage .= "Owner: ".$requester." \n";
+                                        $sMessage .= "Subject : ".$subject."\n\n";
+                                        $sMessage .= "-------------------------- \n";
+                                        $sMessage .= "คำแนะ/แก้ไข : ".$add_task."\n\n";
 
-                                        // $sMessage .= "ติดตามงานได้ที่ Link Web: http://58.137.58.163/pms/view.php?id=$_GET[id] \n\n";
-                                        // $sMessage .= "@All \n";
+                                        $sMessage .= "ติดตามงานได้ที่ Link Web: http://58.137.58.163/pms/view.php?id=$_GET[id] \n\n";
+                                        $sMessage .= "@All \n";
                             
                                         
-                                        // $chOne = curl_init(); 
-                                        // curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
-                                        // curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
-                                        // curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
-                                        // curl_setopt( $chOne, CURLOPT_POST, 1); 
-                                        // curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=".$sMessage); 
-                                        // $headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$sToken.'', );
-                                        // curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
-                                        // curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
-                                        // $resultt1 = curl_exec( $chOne ); 
+                                        $chOne = curl_init(); 
+                                        curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+                                        curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
+                                        curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
+                                        curl_setopt( $chOne, CURLOPT_POST, 1); 
+                                        curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=".$sMessage); 
+                                        $headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$sToken.'', );
+                                        curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
+                                        curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
+                                        $resultt1 = curl_exec( $chOne ); 
 
 
                                 //     // echo "<script>alert('ยินดีตอนรับ Admin เข้าสู่ระบบ'); window.location='../index.php'</script>";
